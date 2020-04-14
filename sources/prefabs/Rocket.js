6e43e98fc1f650821c76b3022df48fb5943b07b7
clairec98 <63259884+clairec98@users.noreply.github.com>
2020-04-13 17:36:29 -0700

    Added assets, timer, and restart

--- sources/prefabs/Rocket.js
+++ sources/prefabs/Rocket.js
@@ -2,6 +2,7 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
+        this.sfxRocket = scene.sound.add('sfx_rocket');
 
         // add an object to existing scene, displayList, updateList
         scene.add.existing(this);
@@ -19,18 +20,24 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
        
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
             this.isFiring = true;
+            this.sfxRocket.play();
         }
         // if fired, move up
         if(this.isFiring && this.y >= 108){
             this.y -= 2;
         }
         // reset on miss
-        if(this.y <= 108){
-            this.isFiring = false;
-            this.y = 431;
-        }
+       if(this.y <= 108){
+           this.reset();
+       } 
         
     }
-}
+
+    // reset function
+    reset(){
+        this.isFiring = false;
+        this.y = 431;
+        }
+ }
